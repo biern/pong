37f3ea003473c27932c5613fab9556eaa8e323b4
biern <biern.m@gmail.com>
2011-02-04 19:53:50 +0100

    Client update

--- client/client.js
+++ client/client.js
@@ -2,14 +2,52 @@ YUI.add("client", function (Y) {
   Y.namespace("Pong").Client = Y.Base.create(
     "Client", Y.Base, [],
     {
-      initializer: function(){
-	alert("Witam i o zdrowie pytam");
+      _connectionHandler: null,
+      // Interface
+      initializer: function(options){
+	this._initConnectionHandler();
+      },
+      connect: function(url){
+	this._connectionHandler.connect(url);
+      },
+      authenticate: function(authData){
+	this._connectionHandler.authenticate(authData);
+      },
+      requestGame: function(playerID){
+	// if playerID is undefined, then random game is requested.
+	this._connectionHandler.requestGame(playerID);
+      },
+      quitGame: function(){
+	
+      },
+      playerMove: function(moveData){
+	
+      },
+      log: function(msg, type){
+	if(! type){
+	  type = "debug";
+	}
+	Y.log(msg, type, "client");
+      },
+      // internals
+      _initConnectionHandler: function(){
+	var that = this;
+	var ch = this._connectionHandler = new Y.Pong._ConnectionHandler();
+	ch.addTarget(this);
+      },
+      _commandNotImplemented: function(cmd, data){
+	
       }
     },
     {
-
+      ATTRS: {
+	ingame: {
+          validator: Y.Lang.isBoolean,
+	  value: false,
+	  readOnly: true
+	}
+      }
     }
   );
-    
   Y.log("module loaded", "debug", "client");
-}, "0", { requires:[] });
+}, "0", { requires: ["connectionhandler"] });
